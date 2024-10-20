a8bf1cc69f58d8742442a3c47f6efba6366182e8
dickwyn <dickwyn@outlook.com>
2024-10-20 12:20:53 -0700

    feat: configure favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,10 +12,12 @@ export const metadata: Metadata = {
     default: 'Dick Wyn Yong',
     template: '%s | Dick Wyn Yong',
   },
-  description: 'Dick Wyn is a software engineer and content creator. He is a Malaysian living in America and enjoys experimenting with new recipes in the kitchen and geeking out on the latest tech gadgets.',
+  description:
+    'Dick Wyn is a software engineer and content creator. He is a Malaysian living in America and enjoys experimenting with new recipes in the kitchen and geeking out on the latest tech gadgets.',
   openGraph: {
     title: 'Dick Wyn Yong',
-    description: 'Dick Wyn is a software engineer and content creator. He is a Malaysian living in America and enjoys experimenting with new recipes in the kitchen and geeking out on the latest tech gadgets.',
+    description:
+      'Dick Wyn is a software engineer and content creator. He is a Malaysian living in America and enjoys experimenting with new recipes in the kitchen and geeking out on the latest tech gadgets.',
     url: baseUrl,
     siteName: 'Dick Wyn Yong',
     locale: 'en_US',
@@ -38,7 +40,15 @@ const cx = (...classes) => classes.filter(Boolean).join(' ');
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={cx('text-black bg-white dark:text-white dark:bg-black')}>
+    <html lang="en" className={cx('text-black bg-white dark:text-wh~ite dark:bg-black')}>
+      <head>
+        <link rel="icon" type="image/png" href="/favicon-48x48.png" sizes="48x48" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="MyWebSite" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <Navbar />
