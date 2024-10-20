1c3d4c6f036bd32241ec26c8388ba9675e81b539
dickwyn <dickwyn@outlook.com>
2024-10-20 15:42:44 -0700

    feat: only console.log site info in the production build

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,18 +13,21 @@ const cx = (...classes) => classes.filter(Boolean).join(' ');
 
 export default function Layout({ children }: { children: ReactNode }) {
   useEffect(() => {
-    console.log(
-      `%cDick Wyn Yong's Portfolio vAPP_VERSION
+    if (process.env.NODE_ENV === 'production') {
+      console.log(
+        `
+%cDick Wyn Yong's Portfolio vAPP_VERSION
   
-  Looking for something?
+Looking for something?
   
-  👨‍💻 https://www.github.com/dickwyn
-  📄 https://dickwyn.xyz/dickwyn-resume.pdf
-  🐛 https://dickwyn.xyz/debug
+👨‍💻 https://www.github.com/dickwyn
+📄 https://dickwyn.xyz/dickwyn-resume.pdf
+🐛 https://dickwyn.xyz/debug
   
-  built: unsetTimestamp`,
-      'font-family:monospace;'
-    );
+built: unsetTimestamp`,
+        'font-family:monospace;'
+      );
+    }
   }, []);
 
   return (
