d69d1aa4b310fed51a3bf6945d81f8d80125f1be
dickwyn <dickwyn@outlook.com>
2024-10-20 15:27:50 -0700

    feat: print out site info in console

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,44 +1,32 @@
+'use client';
+
 import './global.css';
-import type { Metadata } from 'next';
-import { Navbar } from './components/nav';
+
+import { ReactNode, useEffect } from 'react';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
-import Footer from './components/footer';
-import { baseUrl } from './sitemap';
 
-export const metadata: Metadata = {
-  metadataBase: new URL(baseUrl),
-  title: {
-    default: 'Dick Wyn Yong',
-    template: '%s | Dick Wyn Yong',
-  },
-  description:
-    'Dick Wyn is a software engineer and content creator. He is a Malaysian living in America and enjoys experimenting with new recipes in the kitchen and geeking out on the latest tech gadgets.',
-  openGraph: {
-    title: 'Dick Wyn Yong',
-    description:
-      'Dick Wyn is a software engineer and content creator. He is a Malaysian living in America and enjoys experimenting with new recipes in the kitchen and geeking out on the latest tech gadgets.',
-    url: baseUrl,
-    siteName: 'Dick Wyn Yong',
-    locale: 'en_US',
-    type: 'website',
-  },
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
-    },
-  },
-};
+import { Navbar } from './components/nav';
+import Footer from './components/footer';
 
 const cx = (...classes) => classes.filter(Boolean).join(' ');
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function Layout({ children }: { children: ReactNode }) {
+  useEffect(() => {
+    console.log(
+      `%cDick Wyn Yong's Portfolio vAPP_VERSION
+  
+  Looking for something?
+  
+  👨‍💻 https://www.github.com/dickwyn
+  📄 https://dickwyn.xyz/dickwyn-resume.pdf
+  🐛 https://dickwyn.xyz/debug
+  
+  built: unsetTimestamp`,
+      'font-family:monospace;'
+    );
+  }, []);
+
   return (
     <html lang="en" className={cx('text-black bg-white dark:text-wh~ite dark:bg-black')}>
       <head>
---
+++ app/metadata.ts
@@ -0,0 +1,32 @@
+import type { Metadata } from 'next';
+import { baseUrl } from './sitemap';
+
+export const metadata: Metadata = {
+  metadataBase: new URL(baseUrl),
+  title: {
+    default: 'Dick Wyn Yong',
+    template: '%s | Dick Wyn Yong',
+  },
+  description:
+    'Dick Wyn is a software engineer and content creator. He is a Malaysian living in America and enjoys experimenting with new recipes in the kitchen and geeking out on the latest tech gadgets.',
+  openGraph: {
+    title: 'Dick Wyn Yong',
+    description:
+      'Dick Wyn is a software engineer and content creator. He is a Malaysian living in America and enjoys experimenting with new recipes in the kitchen and geeking out on the latest tech gadgets.',
+    url: baseUrl,
+    siteName: 'Dick Wyn Yong',
+    locale: 'en_US',
+    type: 'website',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+};
