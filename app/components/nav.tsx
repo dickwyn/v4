1fe9459b0628ae60b66c1d9974e8432b9f87001f
dickwyn <dickwyn@outlook.com>
2024-11-08 19:33:39 -0800

    style: red underline on hovered links

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -20,7 +20,7 @@ export function Navbar() {
   const colorScheme = usePrefersColorScheme();
 
   return (
-    <aside className="-ml-[8px] mb-8">
+    <aside className="-ml-[8px] mb-8 text-lg">
       <div className="lg:sticky lg:top-20">
         <nav
           className="flex flex-row items-center relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
@@ -40,7 +40,7 @@ export function Navbar() {
                 <Link
                   key={path}
                   href={path}
-                  className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1"
+                  className="transition-all hover:underline underline-offset-3 hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1"
                 >
                   {name}
                 </Link>
