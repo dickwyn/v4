11c64f18a3bf721a3dc721ae7132b9b8a6408203
dickwyn@dev0 <dickwynyong@microsoft.com>
2025-01-19 18:59:58 +0800

    chore: update bio in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,35 +2,38 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-5 text-3xl">
-        Dickwyn <b>Yong</b>
+        hi, i&apos;m <b>dickwyn</b>
       </h1>
       <section className="grid gap-2">
         <p>
-          i&apos;m a software engineer at{' '}
+          i&apos;m a software engineer passionate about creating delightful web experiences. with 5+
+          years of experience, i currently build fraud prevention solutions to protect{' '}
           <a href="https://microsoft.com" target="_blank" rel="noopener noreferrer">
             Microsoft
           </a>
+          &apos;s digital ecosystem.
         </p>
         <p>
-          i was born and raised in malaysia and got my computer science degree from{' '}
-          <a href="https://www.asu.edu/" target="_blank" rel="noopener noreferrer">
+          i was born and raised in{' '}
+          <a href="https://www.malaysia.travel" target="_blank" rel="noopener noreferrer">
+            Malaysia
+          </a>{' '}
+          and earned my computer science degree from{' '}
+          <a href="https://www.asu.edu" target="_blank" rel="noopener noreferrer">
             ASU
           </a>
         </p>
         <p>
-          i&apos;m currently into running and plane photography but i used to upload videos on my{' '}
+          right now, i&apos;m all about running—working my way up to a marathon. i used to share how
+          technology impacts my everyday life on my{' '}
           <a href="https://www.youtube.com/dickwyn" target="_blank" rel="noopener noreferrer">
             YouTube
           </a>{' '}
-          channel about how technology gets applied in my everyday life.
+          channel, but now i&apos;m just running with it (literally).
         </p>
         <p>
-          my weekly schedule minus sleeping comprises 30% coding, 10% meetings, 10% cooking, 30%
-          watching youtube, and 20% running (plus exercising)
-        </p>
-        <p>i like consumer technology, airplanes, photography and eating + cooking chicken rice</p>
-        <p className="mt-3 italic">
-          p.s. i don&apos;t have a middle name, my first name has two words (Dick Wyn).
+          my interests include consumer technology, airplanes, photography, videography, traveling,
+          and exploring new cuisines.
         </p>
       </section>
     </section>
