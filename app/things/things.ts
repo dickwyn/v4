04bf2cd4ade3f809c5648bf2aa2fe6abaeba2a0c
Dickwyn Yong <11251672+dickwyn@users.noreply.github.com>
2025-01-23 21:46:36 +0800

    chore: remove COVID-19 reference in things.ts

--- app/things/things.ts
+++ app/things/things.ts
@@ -2,7 +2,7 @@ export const things = [
   {
     name: 'every day carry',
     description:
-      'These are the things that I have with me if I am going outside. Obviously not going out that often these days due to the COVID-19 pandemic.',
+      'These are the things that I have with me if I am going outside.',
     sort: true,
     itemList: [
       {
