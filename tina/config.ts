09a9b38d20fafa4710302ef6fda06f2378bee38c
dickwyn <dickwyn@outlook.com>
2025-08-15 09:55:02 -0700

    feat: add tinacms

---
+++ tina/config.ts
@@ -0,0 +1,82 @@
+import { defineConfig } from 'tinacms';
+
+const branch = process.env.VERCEL_GIT_COMMIT_REF || process.env.HEAD || 'main';
+
+export default defineConfig({
+  branch,
+  clientId: 'd94a8e77-b70d-45b4-a564-fbecdae73d1d',
+  token: process.env.TINA_TOKEN,
+  build: {
+    outputFolder: 'admin',
+    publicFolder: 'public',
+  },
+  media: {
+    tina: {
+      mediaRoot: 'images',
+      publicFolder: 'public',
+    },
+  },
+  // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
+  schema: {
+    collections: [
+      {
+        name: 'post',
+        label: 'Posts',
+        path: 'content/posts',
+        format: 'mdx',
+        defaultItem: () => {
+          return {
+            title: 'New Post',
+            date: new Date().toISOString(),
+            draft: true,
+          };
+        },
+        fields: [
+          {
+            type: 'string',
+            name: 'title',
+            label: 'Title',
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: 'datetime',
+            name: 'date',
+            label: 'Date',
+            required: true,
+          },
+          {
+            type: 'string',
+            name: 'description',
+            label: 'Description',
+            ui: {
+              component: 'textarea',
+            },
+          },
+          {
+            type: 'string',
+            name: 'slug',
+            label: 'Slug',
+            required: true,
+          },
+          {
+            type: 'boolean',
+            name: 'draft',
+            label: 'Draft',
+            description: 'If this is checked the post will not be published',
+          },
+          {
+            type: 'rich-text',
+            name: 'body',
+            label: 'Body',
+            isBody: true,
+          },
+        ],
+        ui: {
+          // This is a DEMO router. You can remove this to fit your site
+          router: ({ document }) => `/blog/${document._sys.filename}`,
+        },
+      },
+    ],
+  },
+});
